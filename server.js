f4760a349ff451557e896671efbf125c71efa1a0
mariomelchor <mario@mariomelchor.com>
2017-10-19 15:19:22 -0700

    Added handlebars

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 var bodyParser = require('body-parser')
-var cheerio = require("cheerio");
-var exphbs  = require('express-handlebars');
-var express = require("express");
-var mongojs = require("mongojs");
-var mongoose = require("mongoose");
-var request = require("request");
+var cheerio    = require("cheerio");
+var exphbs     = require('express-handlebars');
+var express    = require("express");
+var mongojs    = require("mongojs");
+var mongoose   = require("mongoose");
+var request    = require("request");
 
 // Initialize Express
 var app = express();
+app.use(express.static("public"));
 
 // Database configuration
 var databaseUrl = "mongoscraper";
@@ -16,6 +17,10 @@ var collections = ["articles"];
 // Use mongojs to hook the database to the db variable
 var db = mongojs(databaseUrl, collections);
 
+// Handlebars
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 // This makes sure that any errors are logged if mongodb runs into an issue
 db.on("error", function(error) {
   console.log("Database Error:", error);
@@ -23,7 +28,7 @@ db.on("error", function(error) {
 
 // Routes
 app.get("/", function(req, res) {
-  res.send("Hello world");
+  res.send(index.html);
 });
 
 // Retrieve data from the db
@@ -72,8 +77,8 @@ app.get("/scrape", function(req, res) {
       }
     });
   });
-  // Send a "Scrape Complete" message to the browser
-  res.send("Scrape Complete");
+
+  res.redirect("/");
 });
 
 // Set the app to listen on port 3000
