4b34e56aef536cfab16c8bf7a06280249340b149
mariomelchor <mario@mariomelchor.com>
2017-10-18 18:04:23 -0700

    all and scrape routes working

--- server.js
+++ server.js
@@ -26,6 +26,56 @@ app.get("/", function(req, res) {
   res.send("Hello world");
 });
 
+// Retrieve data from the db
+app.get("/all", function(req, res) {
+  // Find all results from the scrapedData collection in the db
+  db.articles.find({}, function(error, found) {
+    // Throw any errors to the console
+    if (error) {
+      console.log(error);
+    }
+    // If there are no errors, send the data to the browser as json
+    else {
+      res.json(found);
+    }
+  });
+});
+
+// Scrape data and place it into the mongodb
+app.get("/scrape", function(req, res) {
+  request("http://www.ocregister.com/", function(error, response, html) {
+    // Load the html body from request into cheerio
+    var $ = cheerio.load(html);
+    // For each element with a "title" class
+    $(".article-title").each(function(i, element) {
+      // Save the title and href of each item in the current element
+      var title = $(element).attr("title");
+      var link = $(element).attr("href");
+
+      // If this found element had both a title and a link
+      if (title && link) {
+        // Insert the data in the articles collection
+        db.articles.insert({
+          title: title,
+          link: link
+        },
+        function(err, inserted) {
+          if (err) {
+            // Log the error if one is encountered during the query
+            console.log(err);
+          }
+          else {
+            // Otherwise, log the inserted data
+            console.log(inserted);
+          }
+        });
+      }
+    });
+  });
+  // Send a "Scrape Complete" message to the browser
+  res.send("Scrape Complete");
+});
+
 // Set the app to listen on port 3000
 app.listen(3000, function() {
   console.log("App running on port 3000!");
